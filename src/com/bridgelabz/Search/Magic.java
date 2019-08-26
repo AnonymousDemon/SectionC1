cc6caffb3b92e2770a893df4dc8ce0061d3bceca
AnonymousDemon <prathmeshrokade8@gmail.com>
2019-08-26 17:16:13 +0530

    Magic number added

--- src/com/bridgelabz/Search/Magic.java
+++ src/com/bridgelabz/Search/Magic.java
@@ -2,27 +2,21 @@ package com.bridgelabz.Search;
 
 import java.util.Scanner;
 
+import com.bridgelabz.utilityC.Utility;
+
 public class Magic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int x,y,q ;
+		Scanner scanner = new Scanner(System.in);
+
+		int n = Integer.parseInt("3");
+		int range = (int) Math.pow(2, n);
+		System.out.println("Range is 0 to " + range);
+		Utility.magicNumber(range);
 		
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Enter a number");
-        q = scanner.nextInt();
-        y= (int) Math.pow(2, q);
-        int lo =0,hi=y-1,mid=(lo+hi)/2;
-        System.out.println("lo= "+lo+ "mid = " +mid+ "hi= " +hi);
-        int key;
-        System.out.println("enter a key to find");
-        key = scanner.nextInt();
-        for(int i=0;i<q;i++)
-        {
-        	
-        }
-        
+		scanner.close();
 	}
 
 }
